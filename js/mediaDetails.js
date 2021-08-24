13691d8889fa612751c52d7aae08b9866f7ee41d
Arslan Shahid <bcsf18m517@pucit.edu.pk>
2021-08-24 11:09:39 +0500

    Done

--- js/mediaDetails.js
+++ js/mediaDetails.js
@@ -1,9 +1,14 @@
 const targetDiv = document.getElementById("id2");
 const btn = document.getElementById("id1");
+const closeBtn = document.getElementById("closeBtn");
+
 btn.onclick = function() {
-    if (targetDiv.style.display !== "none") {
-        targetDiv.style.display = "none";
-    } else {
+    if (targetDiv.style.display == "none") {
         targetDiv.style.display = "block";
+    } else {
+        targetDiv.style.display = "none";
     }
 };
+closeBtn.onclick = function() {
+    targetDiv.style.display = "none";
+}
