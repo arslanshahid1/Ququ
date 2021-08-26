efab12ef3ccb4dae0be87abea2daf04c884dfa98
Arslan Shahid <bcsf18m517@pucit.edu.pk>
2021-08-26 11:19:00 +0500

    Done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
